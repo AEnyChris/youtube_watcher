1a7ae91771cbda558dbd89e7c9cbe3c1af4f6e75
AEnyChris <enyonejoseph@gmail.com>
2024-04-08 01:44:06 +0100

    made changes to config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 config = {
-    "google_api_key":"AIzaSyBK67zHuPXvYZM8JObJJ_bpuRh7UdEALrI",
-    "playlist_id":"PLcJSLelhBz4fAZmh0dEIb-Gyx-TqQOTMl",
-    "page_token": "EAAajQFQVDpDQVVpRURoQ01qUXdNVGN6TVVJeFJVRkJPVEVvQVVpQzZyT1FsdnVFQTFBQldrUWlRMmxLVVZSSFRrdFZNSGhzWWtkb1EyVnFVbXRaYlZadFpFYzRkMlZwTVVKTk0xSlFUVEZ3VWxnd1VqTmxXRWt6UldkelNYVnhUR0p5ZDFsUk1FcGxSazVSSWc",
+    "google_api_key":"insert google api key here",
+    "playlist_id":"",
+    "page_token": "page token",
     "kafka":{
-        "bootstrap.servers": "pkc-l6wr6.europe-west2.gcp.confluent.cloud:9092",
+        "bootstrap.servers": "insert the link to the serv",
         "security.protocol":"sasl_ssl",
         "sasl.mechanism": "PLAIN",
-        "sasl.username": "QSKNGGDWGB5FNIPR",
-        "sasl.password": "A1xyycXVEJWG8e6Pjz6XqSxFV53vhTtUfknzP7DkR4yICL9F+VeuK94kMGKemoga"
+        "sasl.username": "insert username here",
+        "sasl.password": "insert password or secret here"
     },
     "schema_registry": {
-        "url":"https://psrc-571d82.europe-west2.gcp.confluent.cloud",
-        "basic.auth.user.info":"P7BLVN2GXUHOPSOU:Ws96bMKMJr8ZGYGsRoMpzrYwp1FIMi1XRvfrFr2/8vLvRuxHiN9ODVSvmT1EnX0M"
+        "url":"",
+        "basic.auth.user.info":""
     }
 }
