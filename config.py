e0b0852c0002d0b548ab56860c67d653bdfa4e7c
AEnyChris <enyonejoseph@gmail.com>
2024-04-11 08:42:24 +0100

    modification on config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 config = {
-    "google_api_key":"insert google api key here",
-    "playlist_id":"",
+    "google_api_key":"Insert google api key",
+    "playlist_id":"any playlist id will do",
     "page_token": "page token",
     "kafka":{
-        "bootstrap.servers": "insert the link to the serv",
+        "bootstrap.servers": "insert the link to the server",
         "security.protocol":"sasl_ssl",
         "sasl.mechanism": "PLAIN",
-        "sasl.username": "insert username here",
-        "sasl.password": "insert password or secret here"
+        "sasl.username": "insert key",
+        "sasl.password": "insert secret here"
     },
     "schema_registry": {
-        "url":"",
-        "basic.auth.user.info":""
+        "url":"insert schema registry server url",
+        "basic.auth.user.info":"insert schema registry key:insert schema registry secret"
     }
 }
